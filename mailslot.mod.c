a91d09c31a7be4f81064211d5824a7a345b80e49
Mikepicker <michele.rullo90@gmail.com>
2015-10-16 14:26:24 +0200

    first commit

--- mailslot.mod.c
+++ mailslot.mod.c
@@ -27,7 +27,7 @@ __attribute__((section("__versions"))) = {
 	{ 0xdd933ced, __VMLINUX_SYMBOL_STR(cdev_init) },
 	{ 0xeeba85b7, __VMLINUX_SYMBOL_STR(cdev_alloc) },
 	{ 0x29537c9e, __VMLINUX_SYMBOL_STR(alloc_chrdev_region) },
-	{ 0xe914e41e, __VMLINUX_SYMBOL_STR(strcpy) },
+	{ 0x69acdf38, __VMLINUX_SYMBOL_STR(memcpy) },
 	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
 };
@@ -38,4 +38,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "F7D51B5CD90578F51D5E5B6");
+MODULE_INFO(srcversion, "727FB155E68CE99C6C24A6A");
